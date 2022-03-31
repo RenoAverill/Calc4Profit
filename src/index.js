879e7d5305724027582eca5609da3029102bd04e
RenoAverill <reno_averill@yahoo.com>
2022-03-31 13:49:23 -0500

    updated dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import * as ReactDOMClient from 'react-dom/client';
 import HomePage from './Pages/homePage'
+import Login from './Components/userLogin'
+import SignUp from './Components/userSignUp';
 function App({ callback }) {
   
   return (
     <div ref={callback}>  
       <HomePage/>
+      <Login/>
+      <SignUp/>
     </div>
   );
 }
