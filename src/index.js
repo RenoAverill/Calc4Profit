1be18e98ffc61dc10510c5e653478029aabfaad7
RenoAverill <reno_averill@yahoo.com>
2022-03-31 12:54:05 -0500

    updated file structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as ReactDOMClient from 'react-dom/client';
+import HomePage from './Pages/homePage'
+function App({ callback }) {
+  
+  return (
+    <div ref={callback}>  
+      <HomePage/>
+    </div>
+  );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOMClient.createRoot(rootElement);
+root.render(<App callback={() => console.log("renderered")} />);
